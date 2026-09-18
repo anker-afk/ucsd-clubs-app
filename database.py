3fbdff22a85e1338a8e249a5743c0b7c555d31e9
anker-afk <attamsaina@gmail.com>
2026-09-17 19:04:42 -0500

    Modify database connection settings
    Updated the database connection string and user.

--- database.py
+++ database.py
@@ -2,13 +2,13 @@ import psycopg2
 import os
 
 def get_connection():
-    database_url = os.getenv("DATABASE_URL")
+    database_url = os.getenv("postgresql://database_zfnc_user:FJpcdf17vVUEesh758yion3f2vUPz7sI@dpg-dam7p6rm8hqs73cqp1l0-a/database_zfnc")
     if database_url:
         conn = psycopg2.connect(database_url)
     else:
         conn = psycopg2.connect(
             dbname="ucsd_clubs",
-            user="your_mac_username",
+            user="smritiattam",
             password="",
             host="localhost",
             port="5432"
